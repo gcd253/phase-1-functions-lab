ef72ba8ee4c614d6d7155452289e5c3f7f6867c1
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-16 18:23:29 -0400

    commit tree 99859aa636dc15b6b5992b0743fd6cf8cb013180

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupSpot) {
+    if (pickupSpot <= 42) {
+        return 42 - pickupSpot
+    }
+    else return pickupSpot - 42
+
+}
+
+
+
+function distanceFromHqInFeet(pickupSpot) {
+    if (pickupSpot <= 42) {
+        return (42 - pickupSpot) * 264
+
+    }
+    else return (pickupSpot - 42) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start <= destination) {
+        return (destination - start) * 264
+    }
+    return (start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const fareInFeet = distanceTravelledInFeet(start, destination);
+
+    if (fareInFeet < 400) {
+        return 0
+    }
+    else if (fareInFeet < 2000) {
+        return (fareInFeet - 400) * 0.02
+    }
+    else if (fareInFeet < 2500) {
+        return 25
+    }
+    else return 'cannot travel that far'
+
+}
